8ea878c45d5e5ee848a12a55a3170a55a1dc14d3
James Petry <gavilancomun@gmail.com>
2015-05-27 14:22:38 +0100

    draw

--- src/main/scala/com/flowlikeariver/kakuro/Kakuro.scala
+++ src/main/scala/com/flowlikeariver/kakuro/Kakuro.scala
@@ -12,8 +12,8 @@ case class Value(values: Set[Int]) extends Cell
 object Kakuro {
 
   def e() = Empty()
-  def d(down: Int) = Down(down)
-  def a(across: Int) = Across(across)
+  def dd(down: Int) = Down(down)
+  def aa(across: Int) = Across(across)
   def da(down: Int, across: Int) = DownAcross(down, across)
   def v() = Value(Set(1, 2, 3, 4, 5, 6, 7, 8, 9))
   def v(values: Set[Int]) = Value(values)
@@ -23,7 +23,12 @@ object Kakuro {
     case Down(n)          => "   %2d\\--  ".format(n)
     case Across(n)        => "   --\\%2d  ".format(n)
     case DownAcross(d, a) => "   %2d\\%2d  ".format(d, a)
-    case Value(vs)        => " xxxxxxxxx"
+    case Value(vs)        => if (1 == vs.size) {
+      vs.map(x => "     " + "%d".format(x) + "    ").mkString("")
+    }
+    else {
+      " " +  List(1, 2, 3, 4, 5, 6, 7, 8, 9).map(x => if (vs(x)) "%d".format(x) else ".").mkString("")
+    }
   }
 
   def main(args: Array[String]) {
--- src/test/scala/com/flowlikeariver/kakuro/TestKakuro.scala
+++ src/test/scala/com/flowlikeariver/kakuro/TestKakuro.scala
@@ -11,15 +11,27 @@ class TestKakuro extends UnitSpec {
   }
   
   "An across cell" should "draw" in {
-    assert("   --\\ 9  " == draw(Kakuro.a(9)))
+    assert("   --\\ 9  " == draw(aa(9)))
   }
   
   "A down cell" should "draw" in {
-    assert("    9\\--  " == draw(Kakuro.d(9)))
+    assert("    9\\--  " == draw(dd(9)))
   }
 
   "A down across cell" should "draw" in {
     assert("    9\\ 3  " == draw(da(9, 3)))
+  }
+
+  "A default value cell" should "draw" in {
+    assert(" 123456789" == draw(v()))
+  }
+
+  "A 2+ value cell" should "draw" in {
+    assert(" 1.3.5...9" == draw(v(Set(1, 3, 5, 9))))
+  }
+
+  "A 1 value cell" should "draw" in {
+    assert("     1    " == draw(v(Set(1))))
   }
 }
 
